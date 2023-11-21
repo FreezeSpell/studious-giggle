36b82244062837f16ccd3c4b9c41b54c06bee8ad
FreezeSpell <berndvanruremonde@gmail.com>
2023-11-21 14:41:40 +0100

    deleted:    main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print("Je bent echt een dikzak olivier")
-<<<<<<< HEAD
-
-for i in range(100):
-    print("Het klopt!")
-=======
-print("Je bent echt een dikzak Bernd")
->>>>>>> 4cd7251b2b6d8ad402b371fea5e6d47b7ee21bc6
